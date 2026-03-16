65b7d0a74dc8561ac222862be4927664463fefa4
mathcarrot <kangjebin07@inu.ac.kr>
2026-03-16 16:44:39 +0000

    streamlit_app.py 가 이 페이지가 되게 해줘

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("자기 소개")
+
+st.header("강경민")
+st.write("제 이름은 강경민입니다. 저는 인천대학교 수학교육과에 재학 중인 학생입니다.")
+
+
