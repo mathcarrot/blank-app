5eb1fde66eff28b308eef4ed4d3458bf6f3622a4
mathcarrot <kangjebin07@inu.ac.kr>
2026-03-18 01:43:55 +0000

    지금 내용으로 페이지를 바꿔줘

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,9 @@ st.title("자기 소개")
 
 st.header("강경민")
 st.write("제 이름은 강경민입니다. 저는 인천대학교 수학교육과에 재학 중인 학생입니다.")
-
+st.header("")
+st.write("저는 강경민입니다.")
+st.header("입력 요소")
+name = st.text_input("강경민")
+age = st.number_input("28")
 
